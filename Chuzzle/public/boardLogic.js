8688e709e2c6f0dabade1356ec6286805c0d34dd
bhenderson93 <brycehenderson1993@gmail.com>
2022-06-12 12:05:21 -0700

    Queen can now move around board without jumping pieces

--- Chuzzle/public/boardLogic.js
+++ Chuzzle/public/boardLogic.js
@@ -1,22 +1,84 @@
 console.log('Hooked up')
 const generalFEN = "rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR"
-const testFEN = "8/8/8/3Q4/8/8/8/8"
+const testFEN = "8/8/2Q5/3Q4/8/8/8/8"
 
 class GameBoard {
     constructor(initialFEN) {
         this.board = []
         this.pieceArray = this.fenConverter(initialFEN)
+        this.tryMove = []
+        this.captured = []
     }
 
-    pieceAttemptsMoves(piece, start, end) {
+    pieceAttemptsMove() {
+        //console.log(this.tryMove)
+        let start = [ Number(this.tryMove[0][0]) , Number(this.tryMove[0][1]) ]
+        let end = [ Number(this.tryMove[1][0]) , Number(this.tryMove[1][1])]
+        let piece = this.board[start[0]][start[1]]
 
-    }
+        if(typeof(piece) !== 'object'){
+            console.log('Cannot find piece on that square')
+            return 
+        }else{
+            console.log('end is' ,end)
+            piece.makeAllMovesArray()
+            console.log('piece is ',piece)
+            let movePath = false
+            //find the movePath to get the piece from start to end.
+            for(let moveArray of piece.allMoves){
+                for(let array of moveArray){
+                    if(array[0]===end[0] && array[1]===end[1]){
+                        movePath = moveArray
+                        break
+                    }
+                }
+                if(movePath){
+                    console.log('movepath is',movePath)
+                    break
+                }
+            }
+            if(!movePath){
+                console.log('Cannot find path to that square with this piece')
+                return
+            }
+            //movePath should be found now or else the function exits.
+            //time to check whether something is in the way.
+
+            for (let move of movePath){
+                console.log(move)
+                let square = this.board[move[0]][move[1]]
+                console.log(square)
+                if(move[0] === end[0] && move[1]===end[1] && square.side !==piece.side){
+                    //check for captures
+                    this.board[start[0]][start[1]] = ''
+                    this.board[end[0]][end[1]] = piece
+                    piece.location = [end[0],end[1]]
+                    console.log('Move successful')
+                    this.redrawBoard()
+                    break
+                }else{
+                    if(typeof(square) === 'object'){
+                        console.log('Something is in the way.')
+                        return
+                    }
+                }
+            }
+
+
+        }
+
+    } //end of pieceAttemptsMove
 
     redrawBoard() {
         for (let row = 0; row < 8; row++) {
             for (let col = 0; col < 8; col++) {
                 let square = document.getElementById(`${col}${row}`)
-                square.innerText = this.board[col][row] //placeholder for inserting the actual piece
+                if(typeof(this.board[col][row]) === 'object'){
+                    let square = document.getElementById(`${col}${row}`)
+                    square.innerText = this.board[col][row].img
+                }else{
+                    square.innerText = ''
+                }
                 //placeholder for pieces
             }
         }
@@ -37,7 +99,8 @@ class GameBoard {
                 if (this.pieceArray[fenIndex] === ' ') {
                     rowArray.push('')
                 } else {
-                    rowArray.push(this.pieceArray[fenIndex])
+                    //logic for putting in pieces
+                    this.pieceArray[fenIndex] === 'Q' ? rowArray.push(new Queen('White' , [col,row])) : null
                 }
                 fenIndex++
             }
@@ -80,65 +143,83 @@ class GameBoard {
 
 
 class Piece {
-    constructor() {
-        this.name = 'piece',
-            this.side = 'white/black',
-            this.location = ['col', 'row']
-        this.img = '',
-            this.movementStyle = [[1, 0]],
-            this.movementMax = 1,
-            this.allMoves = [],
-            this.initialMoveAvailable = true,
-            this.specialMove1 = true,
+    constructor(side, location) {
+        this.name = 'piece'
+            this.side = side
+            this.location = location
+            this.img = ''
+            this.movementStyle = [[1, 0]]
+            this.movementMax = 1
+            this.allMoves = []
+            this.initialMoveAvailable = true
+            this.specialMove1 = true
             this.specialMove2 = true
     }
     makeAllMovesArray() {
+        this.allMoves = []
         for (let movement of this.movementStyle) {
-            console.log(movement)
+            //console.log(movement)
+            let moveStyleArray = []
             for (let inc = 1; inc <= this.movementMax; inc++) {
                 let tempMove = this.location.slice()
                 tempMove[0] += (movement[0] * inc)
                 tempMove[1] += (movement[1] * inc)
-                console.log('tempmove' , tempMove)
+                //console.log('tempmove', tempMove)
                 if (tempMove[0] >= 0 && tempMove[0] <= 7 && tempMove[1] >= 0 && tempMove[1] <= 7) {
-                    this.allMoves.push(tempMove)
+                    moveStyleArray.push(tempMove)
                 }
             }
+            this.allMoves.push(moveStyleArray)
         }
     }
 }
 
 class Queen extends Piece {
-    constructor() {
-        super(),
-        this.name = 'Queen',
-            this.side = 'White',
-            this.location = [3, 3],
-            this.img = 'Q',
-            this.movementStyle = [[-1, 0], [-1, 1], [0, 1], [1, 1], [1, 0], [1, -1], [0, -1], [-1, -1]],
+    constructor(side, location) {
+        super(side, location)
+            this.name = 'Queen'
+            this.img = 'Q'
+            this.movementStyle = [[-1, 0], [-1, 1], [0, 1], [1, 1], [1, 0], [1, -1], [0, -1], [-1, -1]]
             this.movementMax = 8
     }
+}
 
+class King extends Piece {
 
+}
+
+class Rook extends Piece {
+
+}
+
+class Knight extends Piece {
+
+}
+
+class Bishop extends Piece {
+
+}
+
+class Pawn extends Piece {
 
 }
 
 const initializeScreen = () => {
-    document.addEventListener('mousedown', (e) => {
+    document.getElementById('board-container').addEventListener('mousedown', (e) => {
         console.log(e.target.id)
+        gameBoard.tryMove[0] = e.target.id.split('')
     })
-    document.addEventListener('mouseup', (e) => {
+    document.getElementById('board-container').addEventListener('mouseup', (e) => {
         console.log(e.target.id)
+        gameBoard.tryMove[1] = e.target.id.split('')
+        gameBoard.pieceAttemptsMove()
     })
     //insert element sticks to mouse listener on click somewhere in here
 
     gameBoard.initializeBoard()
 }
 
-const queen = new Queen()
 const gameBoard = new GameBoard(testFEN)
 initializeScreen()
 
-queen.makeAllMovesArray()
-console.log(queen.allMoves)
 
