efb6830f214a39eb97163467ad5f332e1871c65c
bhenderson93 <brycehenderson1993@gmail.com>
2022-06-12 15:15:03 -0700

    Log in place for castling on either side- without castle through check conditions

--- Chuzzle/public/boardLogic.js
+++ Chuzzle/public/boardLogic.js
@@ -1,6 +1,6 @@
 console.log('Hooked up')
 const generalFEN = "rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR"
-const testFEN = "8/8/2Q5/3Q4/8/8/8/8"
+const testFEN = "8/8/8/8/8/8/K7/8"
 
 class GameBoard {
     constructor(initialFEN) {
@@ -8,11 +8,12 @@ class GameBoard {
         this.pieceArray = this.fenConverter(initialFEN)
         this.tryMove = []
         this.captured = []
+        this.enPassant = []
     }
 
     pieceAttemptsMove() {
         //console.log(this.tryMove)
-        let start = [ Number(this.tryMove[0][0]) , Number(this.tryMove[0][1]) ]
+        let start = [ Number(this.tryMove[0][0]) , Number(this.tryMove[0][1])]
         let end = [ Number(this.tryMove[1][0]) , Number(this.tryMove[1][1])]
         let piece = this.board[start[0]][start[1]]
 
@@ -20,8 +21,11 @@ class GameBoard {
             console.log('Cannot find piece on that square')
             return 
         }else{
+            if((piece.side === 'White' && currentTurn !== 0) ||(piece.side === 'Black' && currentTurn !== 1)){
+                console.log('Not your turn!')
+                return
+            }}
             console.log('end is' ,end)
-            piece.makeAllMovesArray()
             console.log('piece is ',piece)
             let movePath = false
             //find the movePath to get the piece from start to end.
@@ -37,8 +41,84 @@ class GameBoard {
                     break
                 }
             }
+            //contains logic for pawn doubleMove, en passant, and castling.
             if(!movePath){
-                console.log('Cannot find path to that square with this piece')
+                if(piece.name === 'Pawn'){
+                    let captureStyle = piece.movementStyle[0]
+                    //statement for capturing a piece by a pawn diagonally
+                    if (typeof(this.board[end[0]][end[1]]) === 'object' && (end[0] === piece.location[0]+captureStyle[0] && (end[1]===piece.location[1] +1 || end[1] === piece.location[1]-1))){
+                        console.log('capturing w/ pawn')
+                        this.board[start[0]][start[1]] = ''
+                        this.captured.push(this.board[end[0]][end[1]])
+                        this.board[end[0]][end[1]] = piece
+                        piece.location = [end[0],end[1]]
+                        piece.initialMoveAvailable = false
+                        console.log('Move successful')
+                        currentTurn ===  0 ? currentTurn = 1 : currentTurn = 0
+                        this.redrawBoard()
+                        return
+                        //statement for capturing a pawn en passant
+                    }else if( ( this.enPassant && end[1] === this.enPassant[1]) && (end[0] === piece.location[0]+captureStyle[0] && (end[1]===piece.location[1] +1 || end[1] === piece.location[1]-1))){
+                        console.log('en passanted')
+                        this.board[start[0]][start[1]] = ''
+                        this.captured.push(this.board[this.enPassant[0]][this.enPassant[1]])
+                        this.board[this.enPassant[0]][this.enPassant[1]] = ''
+                        this.board[end[0]][end[1]] = piece
+                        piece.location = [end[0],end[1]]
+                        piece.initialMoveAvailable = false
+                        currentTurn ===  0 ? currentTurn = 1 : currentTurn = 0
+                        this.enPassant = null
+                        this.redrawBoard()
+                        return
+                    }
+            }
+            else if(piece.initialMoveAvailable && piece.name === 'King' && Math.abs(end[1]-start[1])===2 && end[0]-start[0] ===0 ){
+
+                if( this.board[end[0]][7].initialMoveAvailable && end[1] === 6){
+                        if (this.board[end[0]][5] !== '' || this.board[end[0]][6] !== ''){
+                            //add in logic for castling through check
+                            console.log('Something is blocking castling')
+                            return
+                        }else{
+                            //king stuff
+                            this.board[end[0]][end[1]] = piece
+                            piece.location= end
+                            this.board[start[0]][start[1]]=''
+                            piece.initialMoveAvailable = false
+                            //rook stuff
+                            this.board[end[0]][7].location = [end[0],end[1]-1]
+                            this.board[end[0]][end[1]-1] = this.board[end[0]][7]
+                            this.board[end[0]][7] = ''
+                            //move stuff
+                            console.log('Castled')
+                            currentTurn ===  0 ? currentTurn = 1 : currentTurn = 0
+                            this.redrawBoard()
+                            return
+                        }
+                    }else if(this.board[end[0]][0].initialMoveAvailable && end[1] === 2){
+                    console.log('attempting castle')
+                    if(this.board[end[0]][3] !== '' || this.board[end[0]][2] !== '' || this.board[end[0]][1] !== ''){
+                        console.log('Something is blocking castling')
+                            return
+                    }else{
+                        //king stuff
+                        this.board[end[0]][end[1]] = piece
+                        piece.location = end
+                        this.board[start[0]][start[1]]=''
+                        piece.initialMoveAvailable = false
+                        //rook stuff
+                        this.board[end[0]][0].location = [end[0],end[1]+1]
+                        this.board[end[0]][end[1]+1] = this.board[end[0]][0]
+                        this.board[end[0]][0] = ''
+                        //move stuff
+                        console.log('Castled')
+                        currentTurn ===  0 ? currentTurn = 1 : currentTurn = 0
+                        this.redrawBoard()
+                        return
+                    }
+                }
+            }
+                console.log('Cannot find path to that square with this piece - inside king/pawn logic')
                 return
             }
             //movePath should be found now or else the function exits.
@@ -48,17 +128,33 @@ class GameBoard {
                 console.log(move)
                 let square = this.board[move[0]][move[1]]
                 console.log(square)
-                if(move[0] === end[0] && move[1]===end[1] && square.side !==piece.side){
+                if( move[0] === end[0] && move[1]===end[1] && ( typeof(square) !=='object' || (typeof(square) === 'object' && square.side !== piece.side))){
                     //check for captures
+                    if(typeof(square) === 'object') {
+                        if(piece.name === 'Pawn'){
+                            console.log('A piece is in the way!')
+                            return   
+                        }else{
+                            this.captured.push(square)
+                        }
+                         }
                     this.board[start[0]][start[1]] = ''
                     this.board[end[0]][end[1]] = piece
                     piece.location = [end[0],end[1]]
+                    piece.initialMoveAvailable = false
+                    console.log('math',Math.abs(start[0]-end[0]))
+                    if(piece.name === 'Pawn' && Math.abs(start[0]-end[0])===2){
+                        this.enPassant = end
+                    }else{
+                        this.enPassant = null
+                    }
                     console.log('Move successful')
+                    currentTurn ===  0 ? currentTurn = 1 : currentTurn = 0
                     this.redrawBoard()
-                    break
+                    return
                 }else{
                     if(typeof(square) === 'object'){
-                        console.log('Something is in the way.')
+                        console.log('A piece is in the way!')
                         return
                     }
                 }
@@ -67,18 +163,22 @@ class GameBoard {
 
         }
 
-    } //end of pieceAttemptsMove
+    //end of pieceAttemptsMove
 
     redrawBoard() {
         for (let row = 0; row < 8; row++) {
             for (let col = 0; col < 8; col++) {
+                let piece = this.board[col][row]
                 let square = document.getElementById(`${col}${row}`)
                 if(typeof(this.board[col][row]) === 'object'){
-                    let square = document.getElementById(`${col}${row}`)
-                    square.innerText = this.board[col][row].img
+                    square.innerText = piece.img
+                    square.style.color = piece.side === 'White' ? 'orange' : 'blue'
+
                 }else{
                     square.innerText = ''
                 }
+
+                typeof(this.board[col][row]) ==='object' ? this.board[col][row].makeAllMovesArray() : null
                 //placeholder for pieces
             }
         }
@@ -92,15 +192,26 @@ class GameBoard {
             let rowArray = []
             for (let col = 0; col < 8; col++) {
                 let square = document.createElement('div')
-                square.style.background = col % 2 === row % 2 ? 'white' : 'black'
-                square.id = `${col}${row}`
+                square.style.background = col % 2 === row % 2 ? 'white' : 'rgba(0,0,0,.7)'
+                square.id = `${row}${col}`
                 square.classList.add('square')
                 gameBoard.append(square)
                 if (this.pieceArray[fenIndex] === ' ') {
                     rowArray.push('')
                 } else {
                     //logic for putting in pieces
-                    this.pieceArray[fenIndex] === 'Q' ? rowArray.push(new Queen('White' , [col,row])) : null
+                    this.pieceArray[fenIndex] === 'Q' ? rowArray.push(new Queen('White' , [row,col])) : null
+                    this.pieceArray[fenIndex] === 'K' ? rowArray.push(new King('White' , [row,col])) : null
+                    this.pieceArray[fenIndex] === 'R' ? rowArray.push(new Rook('White' , [row,col])) : null
+                    this.pieceArray[fenIndex] === 'B' ? rowArray.push(new Bishop('White' , [row,col])) : null
+                    this.pieceArray[fenIndex] === 'N' ? rowArray.push(new Knight('White' , [row,col])) : null
+                    this.pieceArray[fenIndex] === 'P' ? rowArray.push(new Pawn('White' , [row,col])) : null
+                    this.pieceArray[fenIndex] === 'q' ? rowArray.push(new Queen('Black' , [row,col])) : null
+                    this.pieceArray[fenIndex] === 'k' ? rowArray.push(new King('Black' , [row,col])) : null
+                    this.pieceArray[fenIndex] === 'r' ? rowArray.push(new Rook('Black' , [row,col])) : null
+                    this.pieceArray[fenIndex] === 'b' ? rowArray.push(new Bishop('Black' , [row,col])) : null
+                    this.pieceArray[fenIndex] === 'n' ? rowArray.push(new Knight('Black' , [row,col])) : null
+                    this.pieceArray[fenIndex] === 'p' ? rowArray.push(new Pawn('Black' , [row,col])) : null
                 }
                 fenIndex++
             }
@@ -141,7 +252,6 @@ class GameBoard {
     }
 }
 
-
 class Piece {
     constructor(side, location) {
         this.name = 'piece'
@@ -155,12 +265,15 @@ class Piece {
             this.specialMove1 = true
             this.specialMove2 = true
     }
+
     makeAllMovesArray() {
+        console.log('in makeAll')
         this.allMoves = []
+        let tempMax = this.name === 'Pawn' ? this.initialMoveAvailable? this.movementMax+1: this.movementMax: this.movementMax
         for (let movement of this.movementStyle) {
             //console.log(movement)
             let moveStyleArray = []
-            for (let inc = 1; inc <= this.movementMax; inc++) {
+            for (let inc = 1; inc <= tempMax; inc++) {
                 let tempMove = this.location.slice()
                 tempMove[0] += (movement[0] * inc)
                 tempMove[1] += (movement[1] * inc)
@@ -172,6 +285,7 @@ class Piece {
             this.allMoves.push(moveStyleArray)
         }
     }
+
 }
 
 class Queen extends Piece {
@@ -185,23 +299,54 @@ class Queen extends Piece {
 }
 
 class King extends Piece {
-
+constructor(side, location) {
+        super(side, location)
+            this.name = 'King'
+            this.img = 'K'
+            this.movementStyle = [[-1, 0], [-1, 1], [0, 1], [1, 1], [1, 0], [1, -1], [0, -1], [-1, -1]]
+            this.movementMax = 1
+    }
 }
 
 class Rook extends Piece {
-
+    constructor(side, location) {
+        super(side, location)
+            this.name = 'Rook'
+            this.img = 'R'
+            this.movementStyle = [[-1, 0], [0, 1], [1, 0], [0, -1]]
+            this.movementMax = 8
+    }
 }
 
 class Knight extends Piece {
-
+    constructor(side, location) {
+        super(side, location)
+            this.name = 'Knight'
+            this.img = 'N'
+            this.movementStyle = [[2,1],[2,-1],[-2,1],[-2,-1],[1,2],[1,-2],[-1,2],[-1,-2]]
+            this.movementMax = 1
+    }
 }
 
 class Bishop extends Piece {
-
+    constructor(side, location) {
+        super(side, location)
+            this.name = 'Bishop'
+            this.img = 'B'
+            this.movementStyle = [ [-1, 1],  [1, 1], [1, -1], [-1, -1]]
+            this.movementMax = 8
+    }
 }
 
 class Pawn extends Piece {
-
+    constructor(side, location) {
+        super(side, location)
+            this.name = 'Pawn'
+            this.img = 'P'
+            this.movementStyle = this.side === 'White' ? [[-1,0]] : [[1,0]]
+            this.movementMax = 1
+            this.initialMoveAvailable = true
+    }
 }
 
 const initializeScreen = () => {
@@ -219,7 +364,8 @@ const initializeScreen = () => {
     gameBoard.initializeBoard()
 }
 
-const gameBoard = new GameBoard(testFEN)
+let currentTurn = 0
+const gameBoard = new GameBoard(generalFEN)
 initializeScreen()
 
 
